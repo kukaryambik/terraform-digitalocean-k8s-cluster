4b19cf86de01488638a7d94cb087aafc0f2ecc59
Aleksandr Iakimenko <kukaryambik@gmail.com>
2023-03-28 15:54:37 +0200

    update outputs

--- outputs.tf
+++ outputs.tf
@@ -64,18 +64,18 @@ output "kube_config" {
 
 output "node_pool" {
   value = {
-    id                = local.k8s.node_pool[0].id
-    actual_node_count = local.k8s.node_pool[0].actual_node_count
-    nodes             = local.k8s.node_pool[0].nodes
-    name              = local.k8s.node_pool[0].name
-    status            = local.k8s.node_pool[0].status
-    droplet_id        = local.k8s.node_pool[0].droplet_id
-    created_at        = local.k8s.node_pool[0].created_at
-    updated_at        = local.k8s.node_pool[0].updated_at
+    id                = local.k8s.node_pool.0.id
+    actual_node_count = local.k8s.node_pool.0.actual_node_count
+    nodes             = local.k8s.node_pool.0.nodes
+    name              = local.k8s.node_pool.0.name
+    status            = local.k8s.node_pool.0.status
+    droplet_id        = local.k8s.node_pool.0.droplet_id
+    created_at        = local.k8s.node_pool.0.created_at
+    updated_at        = local.k8s.node_pool.0.updated_at
     taint = {
-      key    = local.k8s.node_pool[0].taint[0].key
-      value  = local.k8s.node_pool[0].taint[0].value
-      effect = local.k8s.node_pool[0].taint[0].effect
+      key    = local.k8s.node_pool.0.taint.0.key
+      value  = local.k8s.node_pool.0.taint.0.value
+      effect = local.k8s.node_pool.0.taint.0.effect
     }
   }
 }
@@ -88,8 +88,8 @@ output "urn" {
 output "maintenance_policy" {
   description = "A block representing the cluster's maintenance window. Updates will be applied within this window. If not specified, a default maintenance window will be chosen."
   value = {
-    day        = local.k8s.maintenance_policy[0].day
-    duration   = local.k8s.maintenance_policy[0].duration
-    start_time = local.k8s.maintenance_policy[0].start_time
+    day        = local.k8s.maintenance_policy.0.day
+    duration   = local.k8s.maintenance_policy.0.duration
+    start_time = local.k8s.maintenance_policy.0.start_time
   }
 }
